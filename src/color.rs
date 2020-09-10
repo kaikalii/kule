82748ab47772fd5cbab8b36b89f7a3ef9f6b0097
Kai Schmidt <kaikaliischmidt@gmail.com>
2020-09-09 19:34:14 -0700

    add more color functions

--- src/color.rs
+++ src/color.rs
@@ -47,46 +47,65 @@ pub trait Color: Copy {
     {
         C::rgba(self.r(), self.g(), self.b(), self.alpha())
     }
+    fn map_rgb<F>(self, f: F) -> Self
+    where
+        F: Fn(f32) -> f32,
+    {
+        Self::rgba(f(self.r()), f(self.g()), f(self.b()), self.alpha())
+    }
+    fn map_all<F>(self, f: F) -> Self
+    where
+        F: Fn(f32) -> f32,
+    {
+        Self::rgba(f(self.r()), f(self.g()), f(self.b()), f(self.alpha()))
+    }
+    fn map_rgb_other<C, F>(self, other: C, f: F) -> Self
+    where
+        C: Color,
+        F: Fn(f32, f32) -> f32,
+    {
+        Self::rgba(
+            f(self.r(), other.r()),
+            f(self.g(), other.g()),
+            f(self.b(), other.b()),
+            self.alpha(),
+        )
+    }
+    fn map_all_other<C, F>(self, other: C, f: F) -> Self
+    where
+        C: Color,
+        F: Fn(f32, f32) -> f32,
+    {
+        Self::rgba(
+            f(self.r(), other.r()),
+            f(self.g(), other.g()),
+            f(self.b(), other.b()),
+            f(self.alpha(), other.alpha()),
+        )
+    }
     fn min<C>(self, other: C) -> Self
     where
         C: Color,
     {
-        Self::rgba(
-            self.r().min(other.r()),
-            self.g().min(other.g()),
-            self.b().min(other.b()),
-            self.alpha().min(other.alpha()),
-        )
+        self.map_all_other(other, f32::min)
     }
     fn max<C>(self, other: C) -> Self
     where
         C: Color,
     {
-        Self::rgba(
-            self.r().max(other.r()),
-            self.g().max(other.g()),
-            self.b().max(other.b()),
-            self.alpha().max(other.alpha()),
-        )
+        self.map_all_other(other, f32::max)
     }
     fn mul(self, val: f32) -> Self {
-        Self::rgba(
-            self.r() * val,
-            self.g() * val,
-            self.b() * val,
-            self.alpha() * val,
-        )
+        self.map_rgb(|c| c * val)
     }
-    fn mul4<C>(self, other: C) -> Self
+    fn mul_alpha(self, val: f32) -> Self {
+        self.with_alpha(self.alpha() * val)
+    }
+    fn mul_color<C>(self, other: C) -> Self
     where
         C: Color,
     {
-        Self::rgba(
-            self.r() * other.r(),
-            self.g() * other.g(),
-            self.b() * other.b(),
-            self.alpha() * other.alpha(),
-        )
+        self.map_all_other(other, std::ops::Mul::mul)
     }
 }
 
